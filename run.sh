c41e425fde8a44123123b93525e59f96bcdec063
bedadiggelmann <diggelmann.beda@gmail.com>
2019-04-22 15:04:15 +0200

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,3 @@
+cd $HOME
+
+./getFilesFromAPI.sh
---
+++ storeFilesInHDFS.sh
@@ -0,0 +1,37 @@
+cd $(dirname $0)
+
+hdfs dfs -mkdir /user/${USER}/examples/unibit.ai
+hdfs dfs -mkdir /user/${USER}/examples/unibit.ai/companyprofile
+hdfs dfs -mkdir /user/${USER}/examples/unibit.ai/companyprofile/csv
+hdfs dfs -mkdir /user/${USER}/examples/unibit.ai/companyprofile/json
+hdfs dfs -mkdir /user/${USER}/examples/unibit.ai/historicalstockprice
+hdfs dfs -mkdir /user/${USER}/examples/unibit.ai/historicalstockprice/csv
+hdfs dfs -mkdir /user/${USER}/examples/unibit.ai/historicalstockprice/json
+
+for gz in companyprofile/data/csv/unibit-ai-companyprofiles*.gz; do
+		fname=$(basename ${gz} .gz)
+		gunzip --stdout ${gz} | hdfs dfs -put -  /user/${USER}/examples/unibit.ai/companyprofile/csv/${fname}
+done
+
+for gz in companyprofile/data/json/*jq.json.gz; do
+		fname=$(basename ${gz} .gz)
+		gunzip --stdout ${gz} | hdfs dfs -put -  /user/${USER}/examples/unibit.ai/companyprofile/json/${fname}
+done
+
+for gz in historicalstockprice/data/csv/unibit-ai-historicalstockprices*.gz; do
+		fname=$(basename ${gz} .gz)
+		gunzip --stdout ${gz} | hdfs dfs -put -  /user/${USER}/examples/unibit.ai/historicalstockprice/csv/${fname}
+done
+
+for gz in historicalstockprice/data/csv/unibit-ai-historicalstockprice-*.gz; do
+		fname=$(basename ${gz} .gz)
+		gunzip --stdout ${gz} | tail -n +2 | hdfs dfs -put -  /user/${USER}/examples/unibit.ai/historicalstockprice/csv/${fname}
+done
+
+for gz in historicalstockprice/data/json/*jq.json.gz; do
+		fname=$(basename ${gz} .gz)
+		gunzip --stdout ${gz} | hdfs dfs -put -  /user/${USER}/examples/unibit.ai/historicalstockprice/json/${fname}
+done
+
+
+hdfs dfs -ls -R /user/${USER}/examples/unibit.ai
