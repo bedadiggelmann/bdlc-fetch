44c1d7c086146468f2c8cff9d7742d15a0320996
Beda <diggelmann.beda@gmail.com>
2019-04-28 18:10:28 +0200

    Änderungen script

--- getFilesFromAPI.sh
+++ getFilesFromAPI.sh
@@ -12,7 +12,7 @@ mkdir -p data/companyprofile/csv/
 mkdir -p data/companyprofile/json/
 #################################################################################################################################################
 # store the data from the API in the created directories
-curl "https://api.unibit.ai/historicalstockprice/AAPL?range=1m&interval=3&AccessKey=demo" > data/historicalstockprice/json/AAPL_stock.json
+curl "https://api.unibit.ai/historicalstockprice/AAPL?range=1m&interval=3&AccessKey=demo" | jq '.[0]' > data/historicalstockprice/json/AAPL_stock.json
 curl "https://api.unibit.ai/historicalstockprice/AAPL?range=1m&interval=3&AccessKey=demo" > data/historicalstockprice/json/AMZN_stock.json
 # curl "https://api.unibit.ai/historicalstockprice/AAPL?range=1y&interval=1&datatype=json&AccessKey=FeVyD9yxVwaP6AGZXNwGm2zMCyuR5ki0" > data/historicalstockprice/json/AAPL_stock.json
 # curl "https://api.unibit.ai/historicalstockprice/AMZN?range=1y&interval=1&datatype=json&AccessKey=FeVyD9yxVwaP6AGZXNwGm2zMCyuR5ki0" > data/historicalstockprice/json/AMZN_stock.json
