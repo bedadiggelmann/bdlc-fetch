8a91dcb57004da76242beb957f5be87f6857168f
Beda <diggelmann.beda@gmail.com>
2019-04-28 17:14:03 +0200

    Änderungen script

--- getFilesFromAPI.sh
+++ getFilesFromAPI.sh
@@ -1,56 +1,54 @@
 #!/usr/bin/env bash
 # 2019-04, Beda Diggelmann, https://github.com/bedadiggelmann
 
-cd$(dirname $0)
+cd $(dirname $0)
 
 #################################################################################################################################################
 # remove directories if they already exist
-rm -rf data/historicalstockprice/csv/
-rm -rf data/historicalstockprice/json/
-rm -rf data/companyprofile/csv/
-rm -rf data/companyprofile/json/
+rm -rf repos/bdlc-fetch/data/historicalstockprice/
+rm -rf repos/bdlc-fetch/data/companyprofile/
 
 #################################################################################################################################################
 # create directories
-mkdir -p data/historicalstockprice/csv/
-mkdir -p data/historicalstockprice/json/
-mkdir -p data/companyprofile/csv/
-mkdir -p data/companyprofile/json/
+mkdir -p repos/bdlc-fetch/data/historicalstockprice/csv/
+mkdir -p repos/bdlc-fetch/data/historicalstockprice/json/
+mkdir -p repos/bdlc-fetch/data/companyprofile/csv/
+mkdir -p repos/bdlc-fetch/data/companyprofile/json/
 
 #################################################################################################################################################
 # store the data from the API in the created directories
+cd
+cd repos/bdlc-fetch/data/historicalstockprice/json/
 
-cd data/historicalstockprice/json/
-wget https://api.unibit.ai/historicalstockprice/AAPL?range=1m&interval=1&datatype=json&AccessKey=FeVyD9yxVwaP6AGZXNwGm2zMCyuR5ki0
 
-# wget 'https://api.unibit.ai/historicalstockprice/AAPL?range=1m&interval=3&AccessKey=demo'
+wget "https://api.unibit.ai/historicalstockprice/AAPL?range=1m&interval=3&AccessKey=demo"
 
-# wget 'https://api.unibit.ai/historicalstockprice/AAPL?range=1y&interval=1&datatype=json&AccessKey=FeVyD9yxVwaP6AGZXNwGm2zMCyuR5ki0'
-# wget 'https://api.unibit.ai/historicalstockprice/AMZN?range=1y&interval=1&datatype=json&AccessKey=FeVyD9yxVwaP6AGZXNwGm2zMCyuR5ki0'
+
+#TEST: wget "https://api.unibit.ai/historicalstockprice/AAPL?range=1m&interval=1&datatype=json&AccessKey=FeVyD9yxVwaP6AGZXNwGm2zMCyuR5ki0"
+
+# wget "https://api.unibit.ai/historicalstockprice/AAPL?range=1y&interval=1&datatype=json&AccessKey=FeVyD9yxVwaP6AGZXNwGm2zMCyuR5ki0"
+# wget "https://api.unibit.ai/historicalstockprice/AMZN?range=1y&interval=1&datatype=json&AccessKey=FeVyD9yxVwaP6AGZXNwGm2zMCyuR5ki0"
 
 cd
+cd repos/bdlc-fetch/data/historicalstockprice/csv/
 
-cd data/historicalstockprice/csv/
+wget "https://api.unibit.ai/historicalstockprice/AAPL?range=3y&interval=100&datatype=csv&AccessKey=demo"
 
-wget https://api.unibit.ai/historicalstockprice/AAPL?range=3y&interval=100&datatype=csv&AccessKey=demo
-
-# wget 'https://api.unibit.ai/historicalstockprice/AAPL?range=1y&interval=1&datatype=csv&AccessKey=FeVyD9yxVwaP6AGZXNwGm2zMCyuR5ki0'
-# wget 'https://api.unibit.ai/historicalstockprice/AMZN?range=1y&interval=1&datatype=csv&AccessKey=FeVyD9yxVwaP6AGZXNwGm2zMCyuR5ki0'
-
-cd
-
-cd data/companyprofile/json/
-
-wget https://api.unibit.ai/companyprofile/AAPL?AccessKey=demo
-
-# wget 'https://api.unibit.ai/companyprofile/AAPL?datatype=json&AccessKey=FeVyD9yxVwaP6AGZXNwGm2zMCyuR5ki0'
-# wget 'https://api.unibit.ai/companyprofile/AMZN?datatype=json&AccessKey=FeVyD9yxVwaP6AGZXNwGm2zMCyuR5ki0'
+# wget "https://api.unibit.ai/historicalstockprice/AAPL?range=1y&interval=1&datatype=csv&AccessKey=FeVyD9yxVwaP6AGZXNwGm2zMCyuR5ki0"
+# wget "https://api.unibit.ai/historicalstockprice/AMZN?range=1y&interval=1&datatype=csv&AccessKey=FeVyD9yxVwaP6AGZXNwGm2zMCyuR5ki0"
 
 cd
+cd repos/bdlc-fetch/data/companyprofile/json/
 
-cd data/companyprofile/csv/
+wget "https://api.unibit.ai/companyprofile/AAPL?AccessKey=demo"
 
-wget https://api.unibit.ai/companyprofile/AAPL?datatype=csv&AccessKey=demo
+# wget "https://api.unibit.ai/companyprofile/AAPL?datatype=json&AccessKey=FeVyD9yxVwaP6AGZXNwGm2zMCyuR5ki0"
+# wget "https://api.unibit.ai/companyprofile/AMZN?datatype=json&AccessKey=FeVyD9yxVwaP6AGZXNwGm2zMCyuR5ki0"
 
-# wget 'https://api.unibit.ai/companyprofile/AAPL?datatype=csv&AccessKey=FeVyD9yxVwaP6AGZXNwGm2zMCyuR5ki0'
-# wget 'https://api.unibit.ai/companyprofile/AMZN?datatype=csv&AccessKey=FeVyD9yxVwaP6AGZXNwGm2zMCyuR5ki0'
+cd
+cd repos/bdlc-fetch/data/companyprofile/csv/
+
+wget "https://api.unibit.ai/companyprofile/AAPL?datatype=csv&AccessKey=demo"
+
+# wget "https://api.unibit.ai/companyprofile/AAPL?datatype=csv&AccessKey=FeVyD9yxVwaP6AGZXNwGm2zMCyuR5ki0"
+# wget "https://api.unibit.ai/companyprofile/AMZN?datatype=csv&AccessKey=FeVyD9yxVwaP6AGZXNwGm2zMCyuR5ki0"
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # 2019-04, Beda Diggelmann, https://github.com/bedadiggelmann
 
-cd$(dirname $0)
+cd $(dirname $0)
 
 ###################################################
 # fetch files from api to local filesystem
