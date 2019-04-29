34876ff08db70288c66c714365d3196350b3e840
Beda <diggelmann.beda@gmail.com>
2019-04-29 13:52:01 +0200

    Änderungen Scripts

--- storeFilesInHDFS.sh
+++ storeFilesInHDFS.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 # 2019-04, Beda Diggelmann, https://github.com/bedadiggelmann
-cd $(dirname $0)
 #############################################################################################################################
 # make the directories in the HDFS
-hdfs dfs -mkdir /user/${USER}/examples/unibit.ai
-hdfs dfs -mkdir /user/${USER}/examples/unibit.ai/companyprofile
-hdfs dfs -mkdir /user/${USER}/examples/unibit.ai/companyprofile/csv
-hdfs dfs -mkdir /user/${USER}/examples/unibit.ai/companyprofile/json
-hdfs dfs -mkdir /user/${USER}/examples/unibit.ai/historicalstockprice
-hdfs dfs -mkdir /user/${USER}/examples/unibit.ai/historicalstockprice/csv
-hdfs dfs -mkdir /user/${USER}/examples/unibit.ai/historicalstockprice/json
+hdfs dfs -mkdir /user/bd01/examples/unibit.ai
+hdfs dfs -mkdir /user/bd01/examples/unibit.ai/companyprofile
+hdfs dfs -mkdir /user/bd01/examples/unibit.ai/companyprofile/csv
+hdfs dfs -mkdir /user/bd01/examples/unibit.ai/companyprofile/json
+hdfs dfs -mkdir /user/bd01/examples/unibit.ai/historicalstockprice
+hdfs dfs -mkdir /user/bd01/examples/unibit.ai/historicalstockprice/csv
+hdfs dfs -mkdir /user/bd01/examples/unibit.ai/historicalstockprice/json
 #############################################################################################################################
 # unzip the files from the local filesytem and put tem into HDFS
 # gunzip --stdout data/companyprofile/json/companyprofiles.jq.json.gz |
@@ -18,4 +17,4 @@ hdfs dfs -mkdir /user/${USER}/examples/unibit.ai/historicalstockprice/json
 # hdfs dfs -put "data/historicalstockprice/json/historicalstockprices.1y.jq.json" "/user/${USER}/examples/unibit.ai/historicalstockprice/json"
 #############################################################################################################################
 # show all the contained files of the unibit.ai directory
-hdfs dfs -ls -R /user/${USER}/examples/unibit.ai
+hdfs dfs -ls -R /user/bd01/examples/unibit.ai
