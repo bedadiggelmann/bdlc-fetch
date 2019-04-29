c7ae8b15633a85e6336bf116486c4e261e14734b
Beda <diggelmann.beda@gmail.com>
2019-04-29 19:30:03 +0200

    Änderungen Scripts

--- testgetfiles.sh
+++ testgetfiles.sh
@@ -41,28 +41,16 @@ curl "https://api.unibit.ai/companyprofile/AMZN?datatype=csv&AccessKey=FeVyD9yxV
 #############################################################
 # CSV: transform companyprofiles
 sed -n '1,1p' unibitdata/companyprofile/csv/AAPL_raw.csv > unibitdata/companyprofile/csv/companyprofiles_header.csv
-sed -n '2,$p' unibitdata/companyprofile/csv/AAPL_raw.csv > unibitdata/companyprofile/csv/AAPL_temp.csv
-sed -n '2,$p' unibitdata/companyprofile/csv/AMZN_raw.csv > unibitdata/companyprofile/csv/AMZN_temp.csv
-cat unibitdata/companyprofile/csv/AAPL_temp.csv unibitdata/companyprofile/csv/AMZN_temp.csv > unibitdata/companyprofile/csv/companyprofiles.csv
+sed -n '2,$p' unibitdata/companyprofile/csv/AAPL_raw.csv > unibitdata/companyprofile/csv/companyprofiles.csv
+sed -n '2,$p' unibitdata/companyprofile/csv/AMZN_raw.csv >> unibitdata/companyprofile/csv/companyprofiles.csv
 rm unibitdata/companyprofile/csv/AAPL_raw.csv
 rm unibitdata/companyprofile/csv/AMZN_raw.csv
-rm unibitdata/companyprofile/csv/AAPL_temp.csv
-rm unibitdata/companyprofile/csv/AMZN_temp.csv
 
 #############################################################
 # CSV: transform stockprices
-sed -n '1,1p' unibitdata/historicalstockprice/csv/AAPL_raw.csv > unibitdata/historicalstockprice/csv/historicalstockprices_1y_header_temp.csv
-sed -n '2,$p' unibitdata/historicalstockprice/csv/AAPL_raw.csv > unibitdata/historicalstockprice/csv/AAPL_temp.csv
-sed -n '2,$p' unibitdata/historicalstockprice/csv/AMZN_raw.csv > unibitdata/historicalstockprice/csv/AMZN_temp.csv
-sed 's/^/ticker,/' unibitdata/historicalstockprice/csv/historicalstockprices_1y_header_temp.csv > unibitdata/historicalstockprice/csv/historicalstockprices_1y_header.csv
-sed 's/^/AAPL,/' unibitdata/historicalstockprice/csv/AAPL_temp.csv > unibitdata/historicalstockprice/csv/AAPL_complete.csv
-sed 's/^/AMZN,/' unibitdata/historicalstockprice/csv/AMZN_temp.csv > unibitdata/historicalstockprice/csv/AMZN_complete.csv
-cat unibitdata/historicalstockprice/csv/AAPL_complete.csv unibitdata/historicalstockprice/csv/AMZN_complete.csv > unibitdata/historicalstockprice/csv/historicalstockprices_1y.csv
+sed -n -e '1,1p' -e 's/^/ticker,/' unibitdata/historicalstockprice/csv/AAPL_raw.csv > unibitdata/historicalstockprice/csv/historicalstockprices_1y_header.csv
+sed -n -e '2,$p' -e 's/^/AAPL,/' unibitdata/historicalstockprice/csv/AAPL_raw.csv > unibitdata/historicalstockprice/csv/historicalstockprices_1y.csv 
+sed -n -e '2,$p' -e 's/^/AMZN,/' unibitdata/historicalstockprice/csv/AMZN_raw.csv >> unibitdata/historicalstockprice/csv/historicalstockprices_1y.csv
 rm unibitdata/historicalstockprice/csv/AAPL_raw.csv
 rm unibitdata/historicalstockprice/csv/AMZN_raw.csv
-rm unibitdata/historicalstockprice/csv/AAPL_temp.csv
-rm unibitdata/historicalstockprice/csv/AMZN_temp.csv
-rm unibitdata/historicalstockprice/csv/AAPL_complete.csv
-rm unibitdata/historicalstockprice/csv/AMZN_complete.csv
-rm unibitdata/historicalstockprice/csv/historicalstockprices_1y_header_temp.csv
 #curl "https://api.unibit.ai/historicalstockprice/AAPL?range=1m&interval=3&AccessKey=demo" | jq -c '. | {ticker: ."Meta Data".ticker, date: ."Stock price"[].date, open: ."Stock price"[].open, high: ."Stock price"[].high, low: ."Stock price"[].low, close: ."Stock price"[].close, adj_close: ."Stock price"[].adj_close, volume: ."Stock price"[].volume}' > data/historicalstockprice/json/AAPL_stock.json
