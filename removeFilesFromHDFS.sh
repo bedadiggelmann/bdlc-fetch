db5f13d1b340b17b6a2374e24d89be2907d64e86
bedadiggelmann <diggelmann.beda@gmail.com>
2019-04-29 16:29:25 +0200

    Delete removeFilesFromHDFS.sh

--- removeFilesFromHDFS.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# 2019-04, Beda Diggelmann, https://github.com/bedadiggelmann
-
-#############################################################################################################################
-# delete the whole unibit.ai directory in HDFS with all its content, show content of /user/${USER}/examples/
-hdfs dfs -rm -r /user/bd01/examples/unibitdata
-hdfs dfs -ls    /user/bd01/examples/
